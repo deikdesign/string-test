f55e68b4adc4f596cc386ac7f6fcabdc22d151ac
Dean <deikdesign@gmail.com>
2021-12-15 23:58:22 +0200

    add tests

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
-myString = 'Very long string 0';
+// myString = 'Very long string 0';
 
-console.log(myString.length);
+// console.log(myString.length);
 
-var n = myString.length; {
-if(n > 1 && n <= 10) n = n.substring(0,10);
-};
-console.log(n);
+// var n = myString.length; {
+// if(n > 1 && n <= 10) n = n.substring(0,10);
+// };
+// console.log(n);
 
+
+function stringLength(n) {
+    if (n.length < 1) {
+        return 'less than 1';
+    } else if (n.length > 10) {
+        return 'more than 10';
+    } else {
+        return n.length;
+    }
+}
+
+module.exports = stringLength;
---
+++ scripts.test.js
@@ -0,0 +1,11 @@
+
+const stringLength = require('./scripts');
+
+describe('myString', () => {
+    test('character', () => {
+        expect(stringLength('hola')).toBe(4);
+      });
+      
+ 
+});
+
